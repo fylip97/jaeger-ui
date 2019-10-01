0436c3fe2f27417e7a7b82872a94a288cc1030a6
Philip <philip.dengler@student-reutlingen.de>
2019-10-01 11:13:50 +0200

    rules

--- packages/jaeger-ui/src/components/TracePage/TraceAnalyze/generateAnaylzeData.tsx
+++ packages/jaeger-ui/src/components/TracePage/TraceAnalyze/generateAnaylzeData.tsx
@@ -8,7 +8,7 @@ export function generateAnalyseData(trace: Trace) {
     analyzeFunction.push({ string: "n+1 Rule: ", function: nPlusOneRule });
     analyzeFunction.push({ string: "\ntimeWastingOperationsRule: ", function: timeWastingOperationsRule });
     analyzeFunction.push({ string: "\npercentage Deviation: ", function: percentageDeviation });
-    analyzeFunction.push({ string: "\nlong databasecall rule: ", function: longDatabaseCall })
+    analyzeFunction.push({ string: "\nlong databasecall Rule: ", function: longDatabaseCall })
     for (var i = 0; i < analyzeFunction.length; i++) {
         resultString = resultString + analyzeFunction[i].string + analyzeFunction[i].function(trace);
     }
@@ -20,6 +20,7 @@ export function generateAnalyseData(trace: Trace) {
  * @param trace whole trace
  */
 function nPlusOneRule(trace: Trace) {
+    var NUMBER_OF_CALLS_THRESHOLD = 10;
     var allSpans = trace.spans;
     var count = 1;
     var sqlCheck = "";
@@ -35,24 +36,23 @@ function nPlusOneRule(trace: Trace) {
             }
         }
     }
-    if (count > 9) {
+    if (count > NUMBER_OF_CALLS_THRESHOLD) {
         return true;
-    } else {
-        return false;
     }
+    return false;
 }
 /**
  * returns true if selfDuration is higher than 80%
  * @param trace whole trace
  */
 function timeWastingOperationsRule(trace: Trace) {
-    var duration = trace.duration;
+    var PERCENT_THRESHOLD = 80;
     var allSpans = trace.spans;
     for (var i = 0; i < allSpans.length; i++) {
         var resultArray = { name: allSpans[i].spanID, count: 0, total: 0, min: allSpans[i].duration, max: 0, self: 0, selfMin: allSpans[i].duration, selfMax: 0, selfAvg: 0, percent: 0 };
         resultArray = calculateContent(allSpans[i], allSpans, resultArray);
-        var percent = resultArray.self / (duration / 100);
-        if (percent > 80) {
+        var percent = resultArray.self / (trace.duration / 100);
+        if (percent > PERCENT_THRESHOLD) {
             return true;
         }
     }
@@ -63,18 +63,19 @@ function timeWastingOperationsRule(trace: Trace) {
  * @param trace whole trace
  */
 function percentageDeviation(trace: Trace) {
+    var PERCENTAGE_DEVIATION_THRESHOLD = 70;
     var allSpans = trace.spans;
     var totalSelf = 0;
-    var allInfos = new Array();
+    var calculatedSpan = new Array();
     for (var i = 0; i < allSpans.length; i++) {
         var resultArray = { name: allSpans[i].spanID, count: 0, total: 0, min: allSpans[i].duration, max: 0, self: 0, selfMin: allSpans[i].duration, selfMax: 0, selfAvg: 0, percent: 0 }
         resultArray = calculateContent(allSpans[i], allSpans, resultArray);
-        allInfos.push(resultArray);
+        calculatedSpan.push(resultArray);
         totalSelf = totalSelf + resultArray.self;
     }
-    for (var i = 0; i < allInfos.length; i++) {
-        var percentageDeviation = ((allInfos[i].self - (totalSelf / allSpans.length)) / allInfos[i].self) * 100;
-        if (percentageDeviation > 70) {
+    for (var i = 0; i < calculatedSpan.length; i++) {
+        var percentageDeviation = ((calculatedSpan[i].self - (totalSelf / allSpans.length)) / calculatedSpan[i].self) * 100;
+        if (percentageDeviation > PERCENTAGE_DEVIATION_THRESHOLD) {
             return true;
         }
     }
@@ -86,13 +87,14 @@ function percentageDeviation(trace: Trace) {
  * @param trace whole trace
  */
 function longDatabaseCall(trace: Trace) {
+    var DATABASE_DURATION_THRESHOLD = 120;
     var allSpans = trace.spans;
     for (var i = 0; i < allSpans.length; i++) {
         for (var j = 0; j < allSpans[i].tags.length; j++) {
             if (allSpans[i].tags[j].key === "sql") {
                 var resultArray = { name: allSpans[i].spanID, count: 0, total: 0, min: allSpans[i].duration, max: 0, self: 0, selfMin: allSpans[i].duration, selfMax: 0, selfAvg: 0, percent: 0 }
                 resultArray = calculateContent(allSpans[i], allSpans, resultArray);
-                if ((Math.round((resultArray.self / 1000) * 100) / 100) > 120) {
+                if ((Math.round((resultArray.self / 1000) * 100) / 100) > DATABASE_DURATION_THRESHOLD) {
                     return true;
                 }
             }
